1fd860509370d81477fa1977010b9464469e0f08
Emmanuel Adelakun <devtenotea@gmail.com>
2023-09-23 22:38:38 +0100

    chore: create dashboard preview

---
+++ src/client/utils/index.util.ts
@@ -0,0 +1,11 @@
+export const dashboardNavigation = [
+  { name: "Dashboard", href: "/_console/admin/dashboard", current: true },
+  { name: "Events", href: "/_console/admin/events", current: false },
+  {
+    name: "Articles",
+    href: "/_console/admin/articles",
+    current: false,
+  },
+  { name: "Members", href: "/_console/admin/members", current: false },
+  { name: "Team", href: "/_console/admin/team", current: false },
+];
